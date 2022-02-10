fdf0974545aee70e98efded2164131baeb72e20f
radik <codefacs@gmail.com>
2022-02-10 14:13:32 +0100

    funziona a meta'

--- electric_site/electronics/urls.py
+++ electric_site/electronics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
-    path('categories/', categories),
-    path('search', data_visualization),
+    path('', home, name='home'),
+    path('categories/', categories, name='categories'),
+    path('search', data_visualization, name='search'),
 ]
--- electric_site/electronics/views.py
+++ electric_site/electronics/views.py
@@ -7,11 +7,20 @@ from . import models
 from .models import Electronics
 
 
+menu = [
+    {'title': 'Home', 'url_name': 'home'},
+    {'title': 'Visual', 'url_name': 'search'},
+    {'title': 'Inserimento', 'url_name': 'categories'}
+]
+
+
 def home(request):
     title = 'Home'
     return render(request, 'electronics/index.html', {'title': title})
 
+
 def data_visualization(request):
+    title = 'Visual'
     if request.GET:
 
         marca: str = request.GET.get('modello')
@@ -21,39 +30,14 @@ def data_visualization(request):
         lst = Electronics.objects.filter(title__istartswith=marca)
         data_s = [(query.title, query.content) for query in lst]
 
-        return render(request, 'electronics/data_visual.html', {'extracted_data': data_s})
-
-
-    return HttpResponse("""<!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="mystyle.css">
-                <title>Document</title>
-        
-            </head>
-            
-            <body>
-            <p><h1>Visualizzazione dati</h1></p>
-                <h2>Cerca dati nel database</h2>
-        
-                <form action="/search">
-                    <label for="modello"> nome del modello:</label>
-                    <input type="text" id="modello" name="modello"><br><br>
-                    <input type="submit" value="Cerca">
-                </form>
-        
-        
-            </body>
-        </html>""")
-
-
+        return render(request, 'electronics/data_visual.html', {'extracted_data': data_s, 'title':title})
+    data_s = []
+    return render(request, 'electronics/data_visual.html', {'extracted_data': data_s, 'title':title})
 
 
 def categories(request):
+    title = "Inserimento"
     if request.GET:
-
         marca: str = request.GET.get('marca').capitalize()
         model: str = request.GET.get('modello').capitalize()
         print(f'{"-" * 50}\n\n')
@@ -62,33 +46,8 @@ def categories(request):
         dt = models.Electronics(title=marca, content=model)
         dt.save()
         print(f'\n\n{"-" * 50}')
-        
+    return render(request, 'electronics/data_insetion.html', {'title':title})
 
-    return HttpResponse("""<!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="mystyle.css">
-                <title>Document</title>
-        
-            </head>
-            
-            <body>
-                <h1>Inserimento dei dati nel database</h1>
-        
-                <form action="/categories">
-                    <label for="marca">Marca:</label>
-                    <input type="text" id="marca" name="marca"><br><br>
-                    <label for="modello">Modello:</label>
-                    <input type="text" id="modello" name="modello"><br><br>
-                    <input type="submit" value="Inserisci">
-                </form>
-        
-                <p>Clicca "Inserisci"</p>
-        
-            </body>
-        </html>""")
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Ops! pagina inesistente!</h1>')
