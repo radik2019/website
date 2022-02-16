0806fa585c51a17b81f677a522113edc044c0f07
radik <codefacs@gmail.com>
2022-02-16 18:56:34 +0100

    aggiunta la tabella OperatingSystem

--- electric_site/electric_site/settings.py
+++ electric_site/electric_site/settings.py
@@ -107,11 +107,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Rome'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- electric_site/electronics/models.py
+++ electric_site/electronics/models.py
@@ -7,10 +7,16 @@ class Electronics(models.Model):
     content = models.TextField(unique=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True, )
+    opsys = models.ForeignKey("OperatingSystem", on_delete=models.PROTECT, null=True)
     
     def get_absolute_url(self):
         return reverse("all_article", kwargs={"article_id": self.pk})
     
     def __str__(self):
         return self.title
+
+
+class OperatingSystem(models.Model):
+    name = models.CharField(max_length=40, db_index=True)
+
 
