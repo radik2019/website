808701c2dbe0b17f0cf217212810c09dbe637b7f
radik <codefacs@gmail.com>
2022-02-17 15:01:10 +0100

    admin

--- electric_site/electronics/models.py
+++ electric_site/electronics/models.py
@@ -3,17 +3,21 @@ from django.urls import reverse
 
 
 class Electronics(models.Model):
-    title = models.CharField(max_length=254)
-    content = models.TextField(unique=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True, )
+    title = models.CharField(max_length=254, verbose_name="Brand")
+    content = models.TextField(unique=True, verbose_name="Modello")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Creato")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Modificato")
     opsys = models.ForeignKey("OperatingSystem", on_delete=models.PROTECT, null=True)
     
     def get_absolute_url(self):
-        return reverse("dettailed", kwargs={"adm": self.pk})
+        return reverse("dettailed", kwargs={"article_id": self.pk})
     
     def __str__(self):
         return self.title
+    
+    class Meta:
+        verbose_name = "Smartphone"
+        ordering = ['time_create', 'title']
 
 
 class OperatingSystem(models.Model):
@@ -21,5 +25,11 @@ class OperatingSystem(models.Model):
     
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = "Sistema operativo"
+        verbose_name_plural = "Sistemi operativi"
+        ordering = ['name']
+        
+        
 
 
